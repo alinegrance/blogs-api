14ddadbe0f6ae6ba4bf6f0356266c0a9bae9a1d1
Aline Grance <grance.aline@gmail.com>
2023-01-27 13:46:58 -0300

    Start project

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ app.use(express.json());
 
 // ...
 
+app.get('/', () => console.log('Starting project'));
+
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
